46848a6e85a8a68709b650027468a1a9eff1dd7c
its-kumar <kumarshanu1009@gmail.com>
2019-12-28 17:08:29 +0530

    Starting pygame tutorial

---
+++ Space Invadors/main.py
@@ -0,0 +1,15 @@
+import pygame
+
+# initialize the pygame
+pygame.init()
+
+# create the screen
+screen = pygame.display.set_mode((800, 600))
+running = True
+
+# Game Loop
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
